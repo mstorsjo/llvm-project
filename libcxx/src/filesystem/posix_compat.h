3e4eee90bed30744fc54d41157274d9e15a1c170
Martin Storsjö <martin@martin.st>
2020-11-09 12:50:43 +0200

    WIP: [libcxx] Make windows remove() use posix semantics if possible

--- libcxx/src/filesystem/posix_compat.h
+++ libcxx/src/filesystem/posix_compat.h
@@ -276,6 +276,15 @@ int remove(const wchar_t *path) {
   detail::WinHandle h(path, DELETE, FILE_FLAG_OPEN_REPARSE_POINT);
   if (!h)
     return set_errno();
+  struct { // FILE_DISPOSITION_INFO_EX
+    DWORD Flags;
+  } infoex;
+  infoex.Flags = 0x3; // FILE_DISPOSITION_FLAG_DELETE | FILE_DISPOSITION_FLAG_POSIX_SEMANTICS
+  if (SetFileInformationByHandle(h, static_cast<FILE_INFO_BY_HANDLE_CLASS>(21) /* FileDispositionInfoEx */, &infoex, sizeof(infoex)))
+    return 0;
+  int e = GetLastError();
+  if (e != ERROR_INVALID_FUNCTION && e != ERROR_INVALID_PARAMETER && e != ERROR_NOT_SUPPORTED)
+    return set_errno();
   FILE_DISPOSITION_INFO info;
   info.DeleteFile = TRUE;
   if (!SetFileInformationByHandle(h, FileDispositionInfo, &info, sizeof(info)))
