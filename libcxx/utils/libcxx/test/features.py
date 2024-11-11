b59c92ca81fd418f6693f1c07dad52f5e75afe79
Martin Storsjö <martin@martin.st>
2024-11-11 23:14:52 +0200

    [libcxx] [test] Remove unused alternate names for locales
    If we detect that either of those alternative locale names can
    be chosen, we set the locale.en_US.UTF-8 feature, which enables
    tests that try to explicitly set and use the "en_US.UTF-8"
    locale.
    
    Thus, if a system only successfully can set a locale with the name
    "English_United States.1252" (but not the others), this doesn't mean
    that we can execute tests that tries enabling the "en_US.UTF-8"
    locale.

--- libcxx/utils/libcxx/test/features.py
+++ libcxx/utils/libcxx/test/features.py
@@ -417,11 +417,11 @@ for macro, feature in inverted_macros.items():
 # names on various systems. Each locale is considered supported if any of the
 # alternative names is supported.
 locales = {
-    "en_US.UTF-8": ["en_US.UTF-8", "en_US.utf8", "English_United States.1252"],
-    "fr_FR.UTF-8": ["fr_FR.UTF-8", "fr_FR.utf8", "French_France.1252"],
-    "ja_JP.UTF-8": ["ja_JP.UTF-8", "ja_JP.utf8", "Japanese_Japan.923"],
-    "ru_RU.UTF-8": ["ru_RU.UTF-8", "ru_RU.utf8", "Russian_Russia.1251"],
-    "zh_CN.UTF-8": ["zh_CN.UTF-8", "zh_CN.utf8", "Chinese_China.936"],
+    "en_US.UTF-8": ["en_US.UTF-8"],
+    "fr_FR.UTF-8": ["fr_FR.UTF-8"],
+    "ja_JP.UTF-8": ["ja_JP.UTF-8"],
+    "ru_RU.UTF-8": ["ru_RU.UTF-8"],
+    "zh_CN.UTF-8": ["zh_CN.UTF-8"],
     "fr_CA.ISO8859-1": ["fr_CA.ISO8859-1", "French_Canada.1252"],
     "cs_CZ.ISO8859-2": ["cs_CZ.ISO8859-2", "Czech_Czech Republic.1250"],
 }
